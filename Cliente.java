f9212d7d366fd9a47823ca3af48ac8d74a6c7a5d
Cauê Klosoviski Stadler de Souza <107008671+cauekssouza@users.noreply.github.com>
2024-05-10 18:59:29 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,3 +1,12 @@
-public class Cliente {
-    
+abstract class Cliente {
+    private String nome;
+    private int cpf;
+
+    public Cliente(String nome, int cpf){
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    // Método Abstrato
+    public abstract void imprimirDados();    
 }
