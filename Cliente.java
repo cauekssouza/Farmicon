3ee8636b4bdb2b0f548ddf53f7524a6e163618cd
Cauê Klosoviski Stadler de Souza <107008671+cauekssouza@users.noreply.github.com>
2024-05-15 17:58:02 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,32 @@
+public class Cliente {
+    private String nome;
+    private String endereco;
+    private int telefone;
+
+    // Método Construtor
+    public Cliente(String nome, String endereco, int telefone){
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    /*Getters e Setters */
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getEndereco(){
+        return endereco;
+    }
+    public void setEndereco(String endereco){
+        this.endereco = endereco;
+    }
+    public int getTelefone(){
+        return telefone;
+    }
+    public void setTelefone(int telefone){
+        this.telefone = telefone;
+    }
+}
