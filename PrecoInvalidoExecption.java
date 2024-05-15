575d2c5b1f9e0508b836f46d2ad68dd05f16b3b4
Cauê Klosoviski Stadler de Souza <107008671+cauekssouza@users.noreply.github.com>
2024-05-15 09:37:17 -0300

    Add files via upload

---
+++ PrecoInvalidoExecption.java
@@ -0,0 +1,13 @@
+// Farmacia não encontrada
+public class PrecoInvalidoExecption  extends Exception{
+    public PrecoInvalidoExecption(String mensagem){
+        super(mensagem);
+    }
+
+    // Exceção para Farmacia que não foi encontrada
+   public class FarmaciaNãoEncontradaException extends Exception{
+    public FarmaciaNãoEncontradaException(String mensagem){
+        super(mensagem);
+    }
+   }
+}
