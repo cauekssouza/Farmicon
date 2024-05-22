aeb003d226b3f6dad3cf8928a7cb4d72803809b7
Cauê Klosoviski Stadler de Souza <107008671+cauekssouza@users.noreply.github.com>
2024-05-22 08:23:38 -0300

    Update FarmaciaNaoEncontradaException.java

--- FarmaciaNaoEncontradaException.java
+++ FarmaciaNaoEncontradaException.java
@@ -1,3 +1,4 @@
+// Classe de Excepção de Não Encontrar tal produto na estebelecimento
 public class FarmaciaNaoEncontradaException extends Exception {
     public FarmaciaNaoEncontradaException(String mensagem) {
         super(mensagem);
