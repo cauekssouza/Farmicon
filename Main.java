f5fdd20f9717cb2341b2c8d4874695955f534b15
Cauê Klosoviski Stadler de Souza <107008671+cauekssouza@users.noreply.github.com>
2024-05-24 08:40:15 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("-------Farmicon--------");
+        System.out.println("Bem-Vindo aqui você pode encontrar todos os remédios que necessita e ainda fazer comparação de preços");
+        System.out.println("-------Farmicon--------");
+    }
+
+}
