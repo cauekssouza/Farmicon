8745c5cc60054eb941957a2acfbde0b580fc2a9a
Cauê Klosoviski Stadler de Souza <107008671+cauekssouza@users.noreply.github.com>
2024-05-29 09:06:19 -0300

    Delete FarmaciaNaoEncontradaException.java

--- FarmaciaNaoEncontradaException.java
+++
@@ -1,6 +0,0 @@
-// Classe de Excepção de Não Encontrar tal produto na estebelecimento
-public class FarmaciaNaoEncontradaException extends Exception {
-    public FarmaciaNaoEncontradaException(String mensagem) {
-        super(mensagem);
-    }
-}
