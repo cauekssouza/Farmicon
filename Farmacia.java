29bd97411b60f2b4fc920df4ef93a25e21172dbf
Cauê Klosoviski Stadler de Souza <107008671+cauekssouza@users.noreply.github.com>
2024-06-05 09:27:26 -0300

    Update Farmacia.java

--- Farmacia.java
+++ Farmacia.java
@@ -4,68 +4,30 @@ import java.util.List;
 public class Farmacia {
     private String nome;
     private String endereco;
-    private String horarioFuncionamento;
-    private List<Medicamento> listaMedicamentos;
+    private String horario;
+    private List<Medicamento> medicamentos;
 
-    public Farmacia(String nome, String endereco, String horarioFuncionamento) {
+    public Farmacia(String nome, String endereco, String horario) {
         this.nome = nome;
         this.endereco = endereco;
-        this.horarioFuncionamento = horarioFuncionamento;
-        this.listaMedicamentos = new ArrayList<>();
+        this.horario = horario;
+        this.medicamentos = new ArrayList<>();
     }
 
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getHorarioFuncionamento() {
-        return horarioFuncionamento;
-    }
-
-    public void setHorarioFuncionamento(String horarioFuncionamento) {
-        this.horarioFuncionamento = horarioFuncionamento;
+    public void adicionarMedicamento(Medicamento medicamento) {
+        medicamentos.add(medicamento);
     }
 
     public List<Medicamento> getListaMedicamentos() {
-        return listaMedicamentos;
-    }
-
-    public void setListaMedicamentos(List<Medicamento> listaMedicamentos) {
-        this.listaMedicamentos = listaMedicamentos;
-    }
-
-    public void adicionarMedicamento(Medicamento medicamento) {
-        listaMedicamentos.add(medicamento);
-    }
-
-    public void removerMedicamento(Medicamento medicamento) {
-        listaMedicamentos.remove(medicamento);
+        return medicamentos;
     }
 
     @Override
     public String toString() {
         return nome;
-    }
-
-    public void adicionarAvaliação(Avaliação avaliação1) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'adicionarAvaliação'");
-    }
-
-    public Avaliação[] getAvaliacoes() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAvaliacoes'");
     }
 }
