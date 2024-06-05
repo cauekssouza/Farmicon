5a4f0cc03a5f9f28a943afba7fe5402108fe9fb5
Cauê Klosoviski Stadler de Souza <107008671+cauekssouza@users.noreply.github.com>
2024-06-05 08:56:29 -0300

    Add files via upload

---
+++ Medicamento.java
@@ -0,0 +1,33 @@
+public class Medicamento {
+    private String nome;
+    private double preco;
+    private Farmacia farmacia;
+    private String localizacao;
+
+    public Medicamento(String nome, double preco, Farmacia farmacia, String localizacao) {
+        this.nome = nome;
+        this.preco = preco;
+        this.farmacia = farmacia;
+        this.localizacao = localizacao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public Farmacia getFarmacia() {
+        return farmacia;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+}
